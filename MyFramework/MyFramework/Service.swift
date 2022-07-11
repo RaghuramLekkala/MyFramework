b097bf4675146c889bc94b173b1769d391d846cb
RAMraghuram <ram309591@gmail.com>
2022-07-12 00:30:30 +0530

    first commit

---
+++ MyFramework/MyFramework/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  MyFramework
+//
+//  Created by Raghuram on 11/07/22.
+//
+
+import Foundation
+
+public class Service{
+    private init() {}
+    public static func doSomething() -> String{
+        return "this is a awesome framework"
+    }
+}
